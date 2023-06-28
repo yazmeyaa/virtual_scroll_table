70bdbc30968c4c3ee01443e8d5167f906b7fdac2
Eugene <evgenijantonenkov456@gmail.com>
2023-06-28 21:48:02 +0300

    codebase update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./index.css";
 
 function generateTableData(rows: number, cols: number) {
@@ -67,7 +67,15 @@ function Table({ data, visibleRows, rowHeight, colWidth }: TableProps) {
             return (
               <tr key={start + rowIdx} style={{ height: rowHeight }}>
                 {row.map((cell, cellIdx) => {
-                  return <td style={{overflow: 'hidden'}} width={colWidth} key={start + "" + rowIdx + cellIdx}>{cell}</td>;
+                  return (
+                    <td
+                      style={{ overflow: "hidden" }}
+                      width={colWidth}
+                      key={start + "" + rowIdx + cellIdx}
+                    >
+                      {cell}
+                    </td>
+                  );
                 })}
               </tr>
             );
@@ -85,24 +93,101 @@ function Table({ data, visibleRows, rowHeight, colWidth }: TableProps) {
 }
 
 function App() {
-  const [data, setData] = useState<string[][]>(generateTableData(200_000, 20));
+  const [data, setData] = useState<string[][]>(generateTableData(200_000, 15));
   const [visibleRows, setVisibleRows] = useState<number>(15);
+  const numberOfVisibleRowsRef = useRef<HTMLInputElement>(null);
+  const numberOfRowsRef = useRef<HTMLInputElement>(null);
 
-  function handleVisibleRowsChange(event: ChangeEvent<HTMLInputElement>) {
-    const num = Number(event.target.value)
-    if(isNaN(num)) return;
-    setVisibleRows(num)
+  function handleVisibleRowsChange() {
+    if (!numberOfVisibleRowsRef.current) return;
+    const inputValue = numberOfVisibleRowsRef.current.value;
+    const number = Number(inputValue);
+    if (isNaN(number) || number <= 0) return;
+    setVisibleRows(number);
+  }
+
+  function handleRowsCountChange() {
+    if (!numberOfRowsRef.current) return;
+    const inputValue = numberOfRowsRef.current.value;
+    const number = Number(inputValue);
+    if (isNaN(number) || number <= 0) return;
+    setData(generateTableData(number, 15));
   }
 
   return (
     <main>
-      <Table visibleRows={visibleRows} data={data} rowHeight={48} colWidth={80} />
-      <div>
-        <div style={{display: 'flex', gap: '1rem'}}>
-          <label htmlFor="visible_rows">
-            Number of visible rows
-          </label>
-          <input id="visible_rows" value={visibleRows} type="number" onChange={handleVisibleRowsChange}/>
+      <Table
+        visibleRows={visibleRows}
+        data={data}
+        rowHeight={48}
+        colWidth={80}
+      />
+      <div
+        style={{
+          marginTop: "1rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          width: "fit-content",
+        }}
+      >
+        <label
+          style={{
+            border: "1px solid black",
+            padding: "0.5rem",
+            borderRight: "none",
+          }}
+          htmlFor="visible_rows"
+        >
+          Number of visible rows
+        </label>
+        <div
+          style={{
+            display: "flex",
+            border: "1px solid black",
+            borderLeft: "none",
+            padding: "0.5rem",
+          }}
+        >
+          <input
+            id="visible_rows"
+            defaultValue={visibleRows}
+            type="number"
+            ref={numberOfVisibleRowsRef}
+          />
+          <button
+            style={{ flexShrink: "1", flexGrow: "1" }}
+            onClick={handleVisibleRowsChange}
+          >
+            Apply
+          </button>
+        </div>
+
+        <label
+          style={{
+            border: "1px solid black",
+            padding: "0.5rem",
+            borderRight: "none",
+          }}
+          htmlFor="rows_count"
+        >
+          Number of rows
+        </label>
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "0.5rem",
+            borderLeft: "none",
+          }}
+        >
+          <input
+            id="rows_count"
+            type="number"
+            defaultValue={data.length}
+            ref={numberOfRowsRef}
+          />
+          <button onClick={handleRowsCountChange}>Create table!</button>
         </div>
       </div>
     </main>
